6074f12f0e2b6539dcb7ccc506461808c2a7fdd9
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-12 17:27:50 +1000

    Edit guest splash page

--- frontend/src/guestApp/splashPage/SplashPage/SplashPage.js
+++ frontend/src/guestApp/splashPage/SplashPage/SplashPage.js
@@ -1,16 +1,64 @@
 import React from 'react';
-import { ChildComponentOne } from '../ChildComponentOne/ChildComponentOne'
-import { ChildComponentTwo } from '../ChildComponentTwo/ChildComponentTwo'
-import webpack from '../../../img/webpack.png'
+import './SplashPage.scss'
 
-const SplashPage = () => (
-  <div>
-    <h2>Splash Page Component</h2>
-    <img src={webpack} alt="webpack" width="300"/>
-    <ChildComponentOne />
-    <ChildComponentTwo />
-    <p>End of Splash Page</p>
-  </div>
-)
+class SplashPage extends React.Component {
+  state = {
+    logInAs: "employee",
+    email: '',
+    password: '' 
+  }
+
+  handleSwitchManager = () => {
+    this.setState((prevState) => {
+      if(prevState.logInAs === "employee") {
+        return {
+          logInAs: "manager"
+        }
+      }
+    })
+  }
+
+  handleSwitchEmployee = () => {
+    this.setState((prevState) => {
+      if(prevState.logInAs === "manager") {
+        return {
+          logInAs: "employee"
+        }
+      }
+    })
+  }
+  
+  handleChange = (e) => {
+    this.setState({
+      email: e.target.value,
+      password: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render() {
+    return  (
+      <div>
+        <img src="" alt="Logo" height="200" width="350"/> <br/>
+        <button onClick={this.handleSwitchEmployee} type="button">Employee</button>
+        <button onClick={this.handleSwitchManager} type="button">Manager</button> <br/>
+        <form onSubmit={this.handleSubmit}>
+          Email <br/>
+          <input onChange={this.handleChange} type="text" name="email"/> <br/>
+          Password <br/>
+          <input onChange={this.handleChange} type="text" name="password"/> <br/>
+          <input type="submit" value="Submit"/> <br/>
+          Can't remember your password?
+        </form>
+      </div>
+    )
+  }
+
+}
+
 
 export { SplashPage }
