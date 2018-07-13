86096ffb14b36c45274df0e33235d17505a3b7b5
oheydrew <drew@oheydrew.me>
2018-07-13 15:32:57 +1000

    Add supertest + first route test!

--- backend/tests/shiftsRouter.test.js
+++ backend/tests/shiftsRouter.test.js
@@ -1,9 +1,13 @@
-const { app } = require('../server')
+require('dotenv').config()
 
-describe('GET /api/', () => {
-  it('responds with json', (done) => {
-    request(app)
-      .get('/user')
+// 'supertest' is a testing tool that lets us send requests- Neato!
+const request = require('supertest')
+const devServer = `http://localhost:${process.env.SERVER_PORT}`
+
+describe('GET /api/shifts/employee', () => {
+  it('Returns response as json', (done) => {
+    request(devServer)
+      .get('/api/shifts/employee')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done)
