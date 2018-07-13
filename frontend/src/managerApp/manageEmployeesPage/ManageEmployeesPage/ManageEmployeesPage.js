7d295f9301e03475ddf3145f52ce4c344b6fa784
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-13 15:42:59 +1000

    Create sortbar component

--- frontend/src/managerApp/manageEmployeesPage/ManageEmployeesPage/ManageEmployeesPage.js
+++ frontend/src/managerApp/manageEmployeesPage/ManageEmployeesPage/ManageEmployeesPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Sortbar from '../SortBar/SortBar'
 
 const ManageEmployeesPage = () => (
   <div>
     <h1>Manage Employees Page</h1>
+    <Sortbar />
   </div>
 )
 
