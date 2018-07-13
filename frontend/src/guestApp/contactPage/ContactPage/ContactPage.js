8f3912a54c46d0906031c8507a39553894d40b20
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-13 13:03:04 +1000

    Create enquiry form

--- frontend/src/guestApp/contactPage/ContactPage/ContactPage.js
+++ frontend/src/guestApp/contactPage/ContactPage/ContactPage.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { ChildComponentOne } from '../ChildComponentOne/ChildComponentOne'
-import { ChildComponentTwo } from '../ChildComponentTwo/ChildComponentTwo'
 import postits from './postits.jpg'
 
 const ContactPage = () => (
   <div>
-    <h2>Contact Page Component</h2>
     <img src={postits} alt="postits" width="300"/>
-    <ChildComponentOne />
-    <ChildComponentTwo />
-    <p>End of Contact Page</p>
+    <h2>Got a burning question?</h2>
+    <form action="" method="post">
+      Email <br/>
+      <input
+        type="email"
+        name="email"
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+        title="Invalid email address"
+        required/> <br/>
+      Reason for enquery <br/>
+      <select name="enquery">
+        <option value="">Select a reson...</option>
+        <option value="reason1">Reason1</option>
+        <option value="reason2">Reason2</option>
+        <option value="reason3">Reason3</option>
+        <option value="reason4">Reason4</option>
+      </select> <br/>
+      Message <br/>
+      <textarea name="message" cols="30" rows="10"></textarea> <br/>
+      <button type="submit">Send</button>
+    </form>
   </div>
 )
 
