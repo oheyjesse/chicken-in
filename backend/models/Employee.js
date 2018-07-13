34af10ab81dfeca4d34d48b854d35410eca9e6ed
oheydrew <drew@oheydrew.me>
2018-07-13 12:20:46 +1000

    Remove business reference (temporarily)

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const employeeSchema = new Schema({
-  name: String,
+  firstName: String,
+  lastName: String,
   email: String,
   password: String, // randomly generate a password upon creation and send emial to employee
   location: [ String ],
   standardRate: Number, // cents
   business: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Business'
+    // type: mongoose.Schema.Types.ObjectId, // Commented out for early testing
+    // ref: 'Business'
+    type: String // Remove this, uncomment next 2 lines
   },
   dateCreated: {
     type: 'Date',
