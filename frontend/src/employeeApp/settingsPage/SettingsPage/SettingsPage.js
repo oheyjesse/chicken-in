693acf977834c7763348f2c226b93b1399110dcb
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-07-13 16:49:19 +1000

    Add change password functionality for employees

--- frontend/src/employeeApp/settingsPage/SettingsPage/SettingsPage.js
+++ frontend/src/employeeApp/settingsPage/SettingsPage/SettingsPage.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const SettingsPage = () => (
-  <div>
-    <h1>Settings Page</h1>
-  </div>
-)
+class SettingsPage extends React.Component {
+  state = {
+    oldPassword: null,
+    retypePassword: null,
+    newPassword: null
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.oldPassword !== this.state.retypePassword) {
+      console.log("Passwords do not match")
+    } else {
+      console.log(this.state)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Change password</h2>
+        <form onSubmit={this.handleSubmit}>
+          Old password
+          <input name="oldPassword" type="text" onChange={this.handleChange} required />
+          Retype Old password
+          <input name="retypePassword" type="text" onChange={this.handleChange} required />
+          New password
+          <input name="newPassword" type="text" onChange={this.handleChange} required />
+          <input type="submit" value="Change Password"/>
+        </form>
+      </div>
+    )
+  }
+  
+}
 
 export { SettingsPage }
