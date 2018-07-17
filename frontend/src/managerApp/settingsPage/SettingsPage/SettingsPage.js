50e33f50e9c8e9f6f2a6f635cbf7167730c6b452
Michael Winter <mcwinter07@gmail.com>
2018-07-17 13:04:48 +1000

    Add ChangePasswordForm to settingsPage

--- frontend/src/managerApp/settingsPage/SettingsPage/SettingsPage.js
+++ frontend/src/managerApp/settingsPage/SettingsPage/SettingsPage.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from 'react'
+import ChangePasswordForm from '../ChangePassword/ChangePasswordForm'
 
 const SettingsPage = () => (
   <div>
     <h1>Settings Page</h1>
+    <ChangePasswordForm />
   </div>
 )
 
