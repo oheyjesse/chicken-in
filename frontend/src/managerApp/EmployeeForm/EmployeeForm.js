1a1bb2676258e8b343cdb449edf4f9b9fe6e37ab
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-17 12:49:22 +1000

    Create Employee form

--- frontend/src/managerApp/EmployeeForm/EmployeeForm.js
+++ frontend/src/managerApp/EmployeeForm/EmployeeForm.js
@@ -6,75 +6,28 @@ class EmployeeForm extends React.Component {
     return (
       <Form>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          <Input type="text" name="firstName" id="firstName" placeholder="First Name..." />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Input type="text" name="lastName" id="lastName" placeholder="Last Name..." />
         </FormGroup>
         <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+          <Input type="email" name="email" id="email" placeholder="Employee Email..." />
+        </FormGroup>
+        <FormGroup>
+          <Input type="select" name="storeLocation" id="storeLocation"> //TODO: select multiple option
+            <option value="">Store Location...</option>
+            <option value="Springvale">Springvale</option>
+            <option value="Hobart">Hobart</option>
+            <option value="Sunshine">Sunshine</option>
           </Input>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleSelectMulti">Select Multiple</Label>
-          <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
+          <Input type="number" step="0.1" name="standardRate" id="standardRate" min="18.90" placeholder="Standard Rate..." />
         </FormGroup>
-        <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-            This is some placeholder block-level help text for the above input.
-            It's a bit lighter and easily wraps to a new line.
-          </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Option one is this and that—be sure to include why it's great
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Option two can be something else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="radio1" disabled />{' '}
-              Option three is disabled
-            </Label>
-          </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            Check me out
-          </Label>
-        </FormGroup>
-        <Button>Submit</Button>
+        <Button>Create New</Button>
       </Form>
-    );
+    )
   }
 }
 
