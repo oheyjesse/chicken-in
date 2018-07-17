0c90558f87684487d60e111e41f3c59a3022b4b6
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-07-17 23:24:22 +1000

    Update domainAndPort

--- frontend/src/domain.js
+++ frontend/src/domain.js
@@ -1,4 +1,5 @@
-const domainAndPort = '178.128.109.115:3000'
+// const domainAndPort = '178.128.109.115:3000'
+const domainAndPort = '178.128.109.115'
 // const domainAndPort = 'localhost:3000'
 
 export { domainAndPort }
