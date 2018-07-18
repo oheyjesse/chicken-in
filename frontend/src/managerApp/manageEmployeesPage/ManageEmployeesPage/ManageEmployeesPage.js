8f27bc065d8f63d11c01f154296cff664a8b650f
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-18 12:48:35 +1000

    Create open/close madal func

--- frontend/src/managerApp/manageEmployeesPage/ManageEmployeesPage/ManageEmployeesPage.js
+++ frontend/src/managerApp/manageEmployeesPage/ManageEmployeesPage/ManageEmployeesPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import EmployeeCard from '../EmployeeCard/EmployeeCard'
 import AddNewEmployeeModal from '../AddNewEmployeeModal/AddNewEmployeeModal'
+import AddNewEmployeeReactModal from '../AddNewEmployeeReactModal/AddNewEmployeeReactModal';
 
 import { dummyData } from '../../../dummyData'
 
 class ManageEmployeesPage extends React.Component {
   state = {
     employees: dummyData,
-    direction: 'asce'
+    direction: 'asce',
+    addNewEmployeeForm: undefined
   }
 
   sortBy = (event) => {
@@ -22,28 +24,40 @@ class ManageEmployeesPage extends React.Component {
           } else {
             if (b[key] < a[key]) return -1
             if (b[key] > a[key]) return 1
-            return 0 
+            return 0
           }
         }),
         direction: this.state.direction === 'asce'
-            ? 'desc'
-            : 'asce' 
+          ? 'desc'
+          : 'asce'
       })
     })
   }
 
-  render() {
+  openNewEmployeeFormModal = () => {
+    this.setState({addNewEmployeeForm: true})
+  }
+
+  closeNewEmployeeFormModal = () => {
+    this.setState({addNewEmployeeForm: false})
+  }
+
+  render () {
     return (
       <div>
         <h1>Manage Employees Page</h1>
         <button onClick={this.sortBy} value='lastName'>Sort by Name</button>
         <button onClick={this.sortBy} value='locations'>Sort by Location</button>
         <button onClick={this.sortBy} value='standardRate'>Sort by Rate/st</button>
-          <AddNewEmployeeModal
-          />
-          <EmployeeCard
-            employees={this.state.employees}
-          />
+        <button onClick={this.openNewEmployeeFormModal}>Add New Employee</button>
+        <AddNewEmployeeReactModal
+          addNewEmployeeForm={this.state.addNewEmployeeForm}
+          closeNewEmployeeFormModal={this.closeNewEmployeeFormModal}/>
+        <AddNewEmployeeModal
+        />
+        <EmployeeCard
+          employees={this.state.employees}
+        />
       </div>
     )
   }
