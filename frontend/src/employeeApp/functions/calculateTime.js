979f891504b587d7bee0231611cbfdd5395ee39b
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-07-20 13:20:58 +1000

    Add wront password message to login screen

---
+++ frontend/src/employeeApp/functions/calculateTime.js
@@ -0,0 +1,20 @@
+const calculateTime = (dayOfWeek, startTimeInMinutesAfterMidnight, endTimeInMinutesAfterMidnight, standardRate, overtimeMultiplier, doubleTimeMultiplier) => {
+  // 1. Calculate total time worked
+  const totalMinutesWorked =  endTimeInMinutesAfterMidnight - startTimeInMinutesAfterMidnight
+
+  // 2.
+
+  // 3.
+
+  // 4.
+
+  const timesAndPayObject = {
+    standardMinutes: null,
+    overtimeMinutes: null,
+    doubleTim1eMinutes: null,
+    totalPay: null,
+  } 
+  return timesAndPayObject
+}
+
+export { calculateTime }
