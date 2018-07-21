3b72ceaf173692cee08b97b75689f5e7098ba83e
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-21 13:33:27 +1000

    Create edit button

--- frontend/src/managerApp/manageEmployeesPage/EmployeeCard/EmployeeCard.js
+++ frontend/src/managerApp/manageEmployeesPage/EmployeeCard/EmployeeCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import EditEmployeeModal from '../EditEmployeeModal/EditEmployeeModal'
+import EditButton from '../../../img/edit-button.svg'
 
 // TODO: replace number with 'business.overtimeMultiplier'
 // TODO: replace number with 'business.doubletimeMultiplier'
@@ -12,12 +13,13 @@ const EmployeeCard = (props) => {
       <p className='currency st'>{(props.employee.standardRate) / 100}</p>
       <p className='currency ot'>{(props.employee.standardRate * 1.5) / 100}</p>
       <p className='currency dt'>{(props.employee.standardRate * 2) / 100}</p>
+      {/* <a href="#edit-button" onClick={props.openEditEmployeeModal}></a> */}
       <button
-        className='edit'
+        className='edit button'
         onClick={(e) => props.openEditEmployeeModal(props.employee.id, e)}
-      >Edit</button>
+      ></button>
       <button
-        className='delete'
+        className='delete button'
         onClick={(e) => props.handleDelete(props.employee.id, e)}
       >&times;</button>
     </div>
