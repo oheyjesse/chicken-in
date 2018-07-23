4b4d975b11d8bb5adcc4bd2c0bb0f5539a4dd233
oheydrew <drew@oheydrew.me>
2018-07-23 15:10:30 +1000

    Manager/Reports Styling tweaks

--- frontend/src/managerApp/reportPage/ReportPage/ReportPage.js
+++ frontend/src/managerApp/reportPage/ReportPage/ReportPage.js
@@ -254,9 +254,9 @@ class ReportPage extends React.Component {
           {/* Area to show totals, using the same filters as the AllShifts component below */}
           <Totals shifts={this.state.allShifts.filter((shift) => {
             const dateFilter = moment(shift.date) >= this.state.paginationWeekStart && moment(shift.date) < this.state.paginationWeekEnd
-            const employeeFilter = this.state.filters.employees.includes(shift.employee.lastName)
-            const locationFilter = this.state.filters.locations.includes(shift.location)
-            const statusFilter = this.state.filters.status.includes(shift.status)
+            const employeeFilter = (this.state.filters.employees.length === 0 ? true : this.state.filters.employees.includes(shift.employee.lastName))
+            const locationFilter = (this.state.filters.locations.length === 0 ? true : this.state.filters.locations.includes(shift.location))
+            const statusFilter = (this.state.filters.status.length === 0 ? true : this.state.filters.status.includes(shift.status))
             return dateFilter && employeeFilter && locationFilter && statusFilter
           })}/>
 
