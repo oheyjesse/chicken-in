be1aa5561a4e83fd149d873ad16db275d1b99052
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-25 13:29:55 +1000

    Change payment formatting to display two decimal places

--- frontend/src/managerApp/manageEmployeesPage/EmployeeCard/EmployeeCard.js
+++ frontend/src/managerApp/manageEmployeesPage/EmployeeCard/EmployeeCard.js
@@ -7,9 +7,9 @@ const EmployeeCard = (props) => {
       <div className='name'>{props.employee.firstName} {props.employee.lastName} </div>
       <div className='email'>{props.employee.email}</div>
       <div className='location'>{props.employee.locations.filter(location => location).join(', ')}</div>
-      <div className='currency st'>{(props.employee.standardRate) / 100}</div>
-      <div className='currency ot'>{(props.employee.standardRate * props.businessData.overtimeMultiplier) / 100}</div>
-      <div className='currency dt'>{(props.employee.standardRate * props.businessData.doubleTimeMultiplier) / 100}</div>
+      <div className='currency st'>{(props.employee.standardRate / 100).toFixed(2)}</div>
+      <div className='currency ot'>{((props.employee.standardRate * props.businessData.overtimeMultiplier) / 100).toFixed(2)}</div>
+      <div className='currency dt'>{((props.employee.standardRate * props.businessData.doubleTimeMultiplier) / 100).toFixed(2)}</div>
       
       {/* these buttons hidden when in desktop via CSS */}
       <div className="btns">
